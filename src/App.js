e4cce6e2691f52e590b8a9d3e79319df1e8e348c
mrobeidat <y.linux96@gmail.com>
2021-09-07 18:22:13 +0300

    finished lab 07

--- src/App.js
+++ src/App.js
@@ -14,16 +14,45 @@ class App extends React.Component {
       lon: '',
       displayName: '',
       mapFlag: false,
-      displayError: false
+      displayError: false,
+      weather: false,
+      weatherArr: [],
+      displayweatherError: false,
+    }
+  }
+
+  getweatherData = async (cityName) => {
+
+    let weatherUrl = `https://city-explorer-api-yousef.herokuapp.com/weather?searchQuery=${cityName}`
+
+    try {
+
+      if (cityName === 'Amman' || cityName === 'Seattle' || cityName === 'Paris') {
+        let weatherData = await axios.get(weatherUrl);
+        this.setState({
+          weatherArr: weatherData.data,
+          weather: true,
+        })
+      } else {
+
+        this.setState({
+          displayweatherError: true,
+        })
+      }
+    }
+    catch {
+      this.setState({
+        displayweatherError: true,
+      })
     }
   }
 
   getLocationData = async (event) => {
     event.preventDefault();
     const cityName = event.target.cityName.value;
-    const myKey = 'pk.527d90a1a6e86da5efab2b12faf50980';
+    this.getweatherData(cityName);
+    const myKey = process.env.REACT_APP_key;
     const URL = `https://eu1.locationiq.com/v1/search.php?key=${myKey}&q=${cityName}&format=json`;
-
     try {
       let resResult = await axios.get(URL);
       this.setState({
@@ -35,7 +64,6 @@ class App extends React.Component {
     }
     catch
     {
-      console.log('err');
       this.setState({
         displayError: true
       })
@@ -48,27 +76,32 @@ class App extends React.Component {
       <>
         <h1>Location App</h1>
 
-        <Form onSubmit={this.locationHandler}>
+        <Form onSubmit={this.getLocationData}>
           <fieldset>
-            <input type='text' name='locationName' placeholder='Enter city name' />
-            <Button style={{ marginLeft: 13}} style={{backgroundColor: '#B61919' }} type='submit'>Search for Location</Button>
+            <input type='text' name='cityName' placeholder='Enter city name' />
+            <Button style={{ marginLeft: 13 }} type='submit'>Search for Location</Button>
           </fieldset>
         </Form>
 
-        {/* <form className="form-box" onSubmit={this.getLocationData}>
-          <input type='text' name='cityName' placeholder='Enter city name' />
-          <button style={{ marginLeft: "5px" }} type='submit'></button>
-        </form> */}
 
         <p>Display name : {this.state.displayName}</p>
         <p>Lat : {this.state.lat}</p>
         <p>Lon : {this.state.lon}</p>
 
-        {this.state.mapFlag &&
-          <Img src={`https://maps.locationiq.com/v3/staticmap?key=pk.527d90a1a6e86da5efab2b12faf50980&center=${this.state.lat},${this.state.lon}& size=400x400`} alt='map' />}
+        {
+          this.state.mapFlag &&
+          <Img src={`https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_key}&center=${this.state.lat},${this.state.lon}& size=400x400`} alt='map' />
+        }
 
         {this.state.displayError && <p>You Have an Error</p>}
-
+        {this.state.weatherArr.map(item => {
+          return (
+            <>
+              <h3>date:{item.date}</h3>
+              <h3>Description:{item.description}</h3>
+            </>
+          )
+        })}
       </>
     )
   }
